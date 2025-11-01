2e4a14f6bcf83038d91056e8277aea26336ff226
Raphael-Maximowski <raphaelmxjv@gmail.com>
2025-11-01 14:09:20 -0300

    chore: Email Exception

--- exceptions/EmailInvalidoException.java
+++ exceptions/EmailInvalidoException.java
@@ -1,9 +1,13 @@
 package exceptions;
 
 public class EmailInvalidoException extends ValidacaoException {
-    
+
     public EmailInvalidoException(String mensagem) {
         super(mensagem);
+    }
+
+    public EmailInvalidoException(String mensagem, Throwable causa) {
+        super(mensagem, causa);
     }
 }
 
